8a3d956c7bac685d7915247e53777def01f2848c
Kia Farhang <kfarhang0@gmail.com>
2016-11-26 20:46:06 -0800

    Updated for Node

--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,51 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
-const uncss = require('gulp-uncss');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const nodemon = require('gulp-nodemon');
 
+gulp.task('css-workflow', function() {
+    gulp.src('./src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('./'))
 
-gulp.task('css-workflow', function(){
-	gulp.src('./src/scss/**/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(cssnano())
-	 	.pipe(sourcemaps.write('./'))
-
-	.pipe(gulp.dest('./dist/css/'))
+    .pipe(gulp.dest('./dist/css/'))
 });
 
-gulp.task('js-workflow', function(){
-	gulp.src('./src/js/**/*.js')
-		.pipe(babel({
-			presets: ['es2015']
-			}))
-		.pipe(uglify())
-	.pipe(gulp.dest('./dist/js/'))
+gulp.task('js-workflow', function() {
+    gulp.src('./src/js/**/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'))
 });
 
-gulp.task('default', function(){
-	gulp.watch('./src/scss/**/*.scss', ['css-workflow']);
-	gulp.watch('./src/js/**/*.js', ['js-workflow']);
+
+gulp.task('watch', function() {
+    gulp.watch('./src/scss/**/*.scss', ['css-workflow']);
+    gulp.watch('./src/js/**/*.js', ['js-workflow']);
 });
+
+gulp.task('nodemon', function() {
+    nodemon({
+            script: 'server.js',
+            ext: 'js scss html',
+            env: {
+                'NODE_ENV': 'development'
+            },
+            ignore: ['dist/**/**'],
+            tasks: ['css-workflow', 'js-workflow']
+        })
+});
+
+gulp.task('default', ['nodemon']);
