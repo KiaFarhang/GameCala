e102046b09bf39d6f5952b5ce644b328be4aea0a
Kia Farhang <kfarhang0@gmail.com>
2016-12-14 15:32:20 -0800

    Scraped a single page

--- scraper.js
+++ scraper.js
@@ -17,20 +17,54 @@ let options = {
 request.get(options, function(error, response, body) {
     if (!error && response.statusCode == 200) {
         jsdom.env(body, function(err, window) {
-            let doc = window.document;
- 
-            let games = doc.getElementsByClassName('search_list_details');
-            // for (let i = 0; i < games.length; i++) {
-            //     let title = games[i].firstChild;
-            //     console.log(title.innerHTML);
-            // }
+            let games = window.document.getElementsByClassName('search_list_details');
+            let obj = {};
 
-            for (var game in games){
-                let html = games[game].innerHTML;
-                console.log(typeof html);
+            for (var game in games) {
+
+                let thisGame = gameParser(games[game]);
+
+                obj[thisGame.title] = thisGame;
             }
+
+            fs.writeFile('test.json', JSON.stringify(obj), function(err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+
         });
 
     }
 
 });
+
+function gameParser(game) {
+    let obj = {};
+    obj.title = game.getElementsByClassName('text_blue')[0].innerHTML;
+    if (game.getElementsByClassName('search_list_tidbit')[0].innerHTML == 'Co-Op') {
+        obj['co-op'] = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[0].getElementsByTagName('div')[1].innerHTML);
+        obj.vs = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[3].getElementsByTagName('div')[1].innerHTML);
+        return obj;
+    }
+    obj.main = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[0].getElementsByTagName('div')[1].innerHTML);
+    obj.extra = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[3].getElementsByTagName('div')[1].innerHTML);
+    obj.completionist = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[6].getElementsByTagName('div')[1].innerHTML);
+    obj.combined = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[9].getElementsByTagName('div')[1].innerHTML);
+
+    return obj;
+
+    function lengthParser(str) {
+        if (str == '--') {
+            return str;
+        }
+
+        if (parseInt(str) != NaN) {
+            if (str.indexOf('½') != -1) {
+                return (parseInt(str) + .5);
+            } else {
+                return parseInt(str);
+            }
+        }
+    }
+}
