82b7d416104ac081b68bfff32045fea85e483ad8
Kia Farhang <kfarhang0@gmail.com>
2016-12-15 16:56:56 -0800

    Scraping operational

--- scraper.js
+++ scraper.js
@@ -6,57 +6,84 @@ const request = require('request');
 const fs = require('fs');
 const jsdom = require('jsdom');
 
-let options = {
-    url: 'http://www.howlongtobeat.com/search_main.php?page=1',
-    method: 'GET',
-    headers: {
-        'Content-Type': 'text/html'
-    }
-};
+let page = 46;
+let gameNumber = 811;
 
-request.get(options, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        jsdom.env(body, function(err, window) {
-            let games = window.document.getElementsByClassName('search_list_details');
-            let obj = {};
 
-            for (var game in games) {
+let CronJob = require('cron').CronJob;
+let job = new CronJob('00 * * * * *', function() {
 
-                let thisGame = gameParser(games[game]);
+    if (page == 1) {
+        let blankObj = {};
+        requestAndParsePage(blankObj);
 
-                obj[thisGame.title] = thisGame;
-            }
+    } else {
+        fs.readFile('games.json', function(error, data) {
+            if (error) console.log(error);
+            let masterObject = JSON.parse(data);
 
-            fs.writeFile('test.json', JSON.stringify(obj), function(err) {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
+            requestAndParsePage(masterObject);
 
         });
-
     }
+}, function() {}, true);
 
-});
+
+function requestAndParsePage(listObject) {
+
+
+    request.get(`http://www.howlongtobeat.com/search_main.php?page=${page}`, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            jsdom.env(body, function(err, window) {
+
+                if (err) {
+                    console.log(`JSdom error: ${err}`);
+                    return;
+                }
+
+                let games = window.document.getElementsByClassName('search_list_details');
+
+                for (var game in games) {
+
+                    listObject[gameNumber] = gameParser(games[game]);
+                    gameNumber++;
+                }
+
+                fs.writeFileSync('games.json', JSON.stringify(listObject));
+                console.log(`Wrote page ${page}`);
+                page++;
+
+            });
+
+        }
+
+    });
+}
+
 
 function gameParser(game) {
-    let obj = {};
-    obj.title = game.getElementsByClassName('text_blue')[0].innerHTML;
-    if (game.getElementsByClassName('search_list_tidbit')[0].innerHTML == 'Co-Op') {
-        obj['co-op'] = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[0].getElementsByTagName('div')[1].innerHTML);
-        obj.vs = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[3].getElementsByTagName('div')[1].innerHTML);
-        return obj;
-    }
-    obj.main = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[0].getElementsByTagName('div')[1].innerHTML);
-    obj.extra = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[3].getElementsByTagName('div')[1].innerHTML);
-    obj.completionist = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[6].getElementsByTagName('div')[1].innerHTML);
-    obj.combined = lengthParser(game.getElementsByClassName('search_list_details_block')[0].getElementsByTagName('div')[9].getElementsByTagName('div')[1].innerHTML);
+    let gameObject = {};
+    gameObject.Title = game.getElementsByClassName('text_blue')[0].innerHTML;
 
-    return obj;
+    let details = game.getElementsByClassName('search_list_details_block')[0];
+    let count = 0;
+
+    while (details.getElementsByTagName('div')[count] != undefined) {
+
+        let nameOfProperty = details.getElementsByTagName('div')[count + 1].innerHTML;
+        gameObject[nameOfProperty] = lengthParser(details.getElementsByTagName('div')[count + 2].innerHTML);
+        count += 3;
+    }
+
+    return gameObject;
 
     function lengthParser(str) {
         if (str == '--') {
             return str;
+        }
+
+        if (str.indexOf('Mins') != -1){
+            return ((parseInt(str) / 60).toFixed(2));
         }
 
         if (parseInt(str) != NaN) {
