ecec24eff8689101d0d11e3e8e65049712db1be1
Kia Farhang <kfarhang0@gmail.com>
2017-01-22 20:32:20 -0800

    Only pull 10 games from DB at a time

--- db_query.js
+++ db_query.js
@@ -12,27 +12,13 @@ let pgConfig = {
 
 let pool = new pg.Pool(pgConfig);
 
-
-// exports.queryLikeTitle = function(string) {
-//     pool.connect(function(error, client, done) {
-//         if (error) throw error;
-//         client.query(`SELECT * FROM games WHERE "Title" LIKE $$%${string}%$$`, function(error, result) {
-//             done();
-//             if (error) console.log(`Error querying DB: ${error}`);
-
-//             console.log(result.rows);
-
-//         });
-//     });
-// }
-
 exports.queryLikeTitle = function queryLikeTitle(string){
     return new Promise((resolve, reject) => {
         pool.connect(function(error, client, done){
             if (error) {
                 return reject(error);
             }
-            client.query(`SELECT * FROM games WHERE "Title" ILIKE $$%${string}%$$`, function(error, result){
+            client.query(`SELECT 10 FROM games WHERE "Title" ILIKE $$%${string}%$$`, function(error, result){
                 done();
                 if (error){
                     return reject(error);
